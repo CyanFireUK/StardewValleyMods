afb1b105c83fb2bda8c433f4bbb4cb716bf43dc6
CyanFireUK <killa796@hotmail.com>
2024-09-27 14:31:12 +0100

    Fixed footstep code triggering outside of multiplayer by mistake which fixes a conflict with Dynamic Reflections

--- ChangeHorseSounds1.6/ModEntry.cs
+++ ChangeHorseSounds1.6/ModEntry.cs
@@ -311,91 +311,94 @@ namespace ChangeHorseSounds
             {
 
                 if (Context.IsMultiplayer)
-                if (__instance.Owner is Farmer farmer && farmer.isRidingHorse() || __instance.Owner == null || __instance.Owner.currentLocation != Game1.currentLocation)
-                    return;
-                Farmer owner = __instance.Owner as Farmer;
-                Vector2 key = owner != null ? owner.Tile : Game1.player.Tile;
-                if (Game1.currentLocation.IsOutdoors || Game1.currentLocation.Name.ToLower().Contains("mine") || Game1.currentLocation.Name.ToLower().Contains("cave") || Game1.currentLocation.IsGreenhouse)
                 {
-                    string str = Game1.currentLocation.doesTileHaveProperty((int)key.X, (int)key.Y, "Type", "Buildings");
-                    if (str == null || str.Length < 1)
-                        str = Game1.currentLocation.doesTileHaveProperty((int)key.X, (int)key.Y, "Type", "Back");
-                    if (str != null)
+                    if (__instance.Owner is Farmer farmer && farmer.isRidingHorse() || __instance.Owner == null || __instance.Owner.currentLocation != Game1.currentLocation)
+                        return;
+                    Farmer owner = __instance.Owner as Farmer;
+                    Vector2 key = owner != null ? owner.Tile : Game1.player.Tile;
+                    if (Game1.currentLocation.IsOutdoors || Game1.currentLocation.Name.ToLower().Contains("mine") || Game1.currentLocation.Name.ToLower().Contains("cave") || Game1.currentLocation.IsGreenhouse)
                     {
-                        if (!(str == "Dirt"))
+                        string str = Game1.currentLocation.doesTileHaveProperty((int)key.X, (int)key.Y, "Type", "Buildings");
+                        if (str == null || str.Length < 1)
+                            str = Game1.currentLocation.doesTileHaveProperty((int)key.X, (int)key.Y, "Type", "Back");
+                        if (str != null)
                         {
-                            if (!(str == "Stone"))
+                            if (!(str == "Dirt"))
                             {
-                                if (!(str == "Grass"))
+                                if (!(str == "Stone"))
                                 {
-                                    if (str == "Wood")
-                                        __instance.currentStep = "woodyStep";
+                                    if (!(str == "Grass"))
+                                    {
+                                        if (str == "Wood")
+                                            __instance.currentStep = "woodyStep";
+                                    }
+                                    else
+                                        __instance.currentStep = Game1.currentLocation.GetSeason() == Season.Winter ? "snowyStep" : "grassyStep";
                                 }
                                 else
-                                    __instance.currentStep = Game1.currentLocation.GetSeason() == Season.Winter ? "snowyStep" : "grassyStep";
+                                    __instance.currentStep = "stoneStep";
                             }
                             else
-                                __instance.currentStep = "stoneStep";
+                                __instance.currentStep = "sandyStep";
+                        }
+                    }
+                    else
+                        __instance.currentStep = "thudStep";
+                    if ((__instance.currentSingleAnimation >= 32 && __instance.currentSingleAnimation <= 56 || __instance.currentSingleAnimation >= 128 && __instance.currentSingleAnimation <= 152) && __instance.currentAnimationIndex % 4 == 0)
+                    {
+                        string cueName = owner.currentLocation.getFootstepSoundReplacement(__instance.currentStep);
+                        if (owner.onBridge.Value)
+                        {
+                            if (owner.currentLocation == Game1.currentLocation && Utility.isOnScreen(owner.Position, 384))
+                                cueName = "thudStep";
+                            owner.bridge?.OnFootstep(owner.Position);
+                        }
+                        if (Game1.currentLocation.terrainFeatures.TryGetValue(key, out TerrainFeature terrainFeature) && terrainFeature is Flooring flooring)
+                            cueName = flooring.getFootstepSound();
+                        Vector2 position = owner.Position;
+                        if (owner.shouldShadowBeOffset)
+                            position += owner.drawOffset;
+                        if (!(cueName == "sandyStep"))
+                        {
+                            if (cueName == "snowyStep")
+                                Game1.currentLocation.temporarySprites.Add(TemporaryAnimatedSprite.GetTemporaryAnimatedSprite("LooseSprites\\Cursors", new Rectangle(247, 407, 6, 6), 2000f, 1, 10000, new Vector2(position.X + 24f + (float)(Game1.random.Next(-4, 4) * 4), position.Y + 8f + (float)(Game1.random.Next(-4, 4) * 4)), false, false, position.Y / 1E+07f, 0.01f, Color.White, (float)(3.0 + Game1.random.NextDouble()), 0.0f, owner.FacingDirection == 1 || owner.FacingDirection == 3 ? -0.7853982f : 0.0f, 0.0f));
                         }
                         else
-                            __instance.currentStep = "sandyStep";
-                    }
-                }
-                else
-                    __instance.currentStep = "thudStep";
-                if ((__instance.currentSingleAnimation >= 32 && __instance.currentSingleAnimation <= 56 || __instance.currentSingleAnimation >= 128 && __instance.currentSingleAnimation <= 152) && __instance.currentAnimationIndex % 4 == 0)
-                {
-                    string cueName = owner.currentLocation.getFootstepSoundReplacement(__instance.currentStep);
-                    if (owner.onBridge.Value)
-                    {
-                        if (owner.currentLocation == Game1.currentLocation && Utility.isOnScreen(owner.Position, 384))
-                            cueName = "thudStep";
-                        owner.bridge?.OnFootstep(owner.Position);
-                    }
-                    if (Game1.currentLocation.terrainFeatures.TryGetValue(key, out TerrainFeature terrainFeature) && terrainFeature is Flooring flooring)
-                        cueName = flooring.getFootstepSound();
-                    Vector2 position = owner.Position;
-                    if (owner.shouldShadowBeOffset)
-                        position += owner.drawOffset;
-                    if (!(cueName == "sandyStep"))
-                    {
-                        if (cueName == "snowyStep")
-                            Game1.currentLocation.temporarySprites.Add(TemporaryAnimatedSprite.GetTemporaryAnimatedSprite("LooseSprites\\Cursors", new Rectangle(247, 407, 6, 6), 2000f, 1, 10000, new Vector2(position.X + 24f + (float)(Game1.random.Next(-4, 4) * 4), position.Y + 8f + (float)(Game1.random.Next(-4, 4) * 4)), false, false, position.Y / 1E+07f, 0.01f, Color.White, (float)(3.0 + Game1.random.NextDouble()), 0.0f, owner.FacingDirection == 1 || owner.FacingDirection == 3 ? -0.7853982f : 0.0f, 0.0f));
+                        {
+                            Game1.currentLocation.temporarySprites.Add(TemporaryAnimatedSprite.GetTemporaryAnimatedSprite("TileSheets\\animations", new Rectangle(128, 2948, 64, 64), 80f, 8, 0, new Vector2(position.X + 16f + (float)Game1.random.Next(-8, 8), position.Y + (float)(Game1.random.Next(-3, -1) * 4)), false, Game1.random.NextBool(), position.Y / 10000f, 0.03f, Color.Khaki * 0.45f, (float)(0.75 + (double)Game1.random.Next(-3, 4) * 0.0500000007450581), 0.0f, 0.0f, 0.0f));
+                            TemporaryAnimatedSprite temporaryAnimatedSprite = TemporaryAnimatedSprite.GetTemporaryAnimatedSprite("TileSheets\\animations", new Rectangle(128, 2948, 64, 64), 80f, 8, 0, new Vector2(position.X + 16f + (float)Game1.random.Next(-4, 4), position.Y + (float)(Game1.random.Next(-3, -1) * 4)), false, Game1.random.NextBool(), position.Y / 10000f, 0.03f, Color.Khaki * 0.45f, (float)(0.550000011920929 + (double)Game1.random.Next(-3, 4) * 0.0500000007450581), 0.0f, 0.0f, 0.0f);
+                            temporaryAnimatedSprite.delayBeforeAnimationStart = 20;
+                            Game1.currentLocation.temporarySprites.Add(temporaryAnimatedSprite);
+                        }
+                        if (cueName != null && owner.currentLocation == Game1.currentLocation && Utility.isOnScreen(owner.Position, 384) && (owner == Game1.player || !LocalMultiplayer.IsLocalMultiplayer(true)))
+                        {
+                            Game1.playSound(cueName);
+                            if (owner.boots.Value != null && owner.boots.Value.ItemId == "853")
+                                Game1.playSound("jingleBell");
+                        }
+                        foreach (Trinket trinketItem in owner.trinketItems)
+                            trinketItem?.OnFootstep(owner);
+                        if (owner.UniqueMultiplayerID != Game1.player.UniqueMultiplayerID)
+                            return;
+                        Game1.stats.takeStep();
                     }
                     else
                     {
-                        Game1.currentLocation.temporarySprites.Add(TemporaryAnimatedSprite.GetTemporaryAnimatedSprite("TileSheets\\animations", new Rectangle(128, 2948, 64, 64), 80f, 8, 0, new Vector2(position.X + 16f + (float)Game1.random.Next(-8, 8), position.Y + (float)(Game1.random.Next(-3, -1) * 4)), false, Game1.random.NextBool(), position.Y / 10000f, 0.03f, Color.Khaki * 0.45f, (float)(0.75 + (double)Game1.random.Next(-3, 4) * 0.0500000007450581), 0.0f, 0.0f, 0.0f));
-                        TemporaryAnimatedSprite temporaryAnimatedSprite = TemporaryAnimatedSprite.GetTemporaryAnimatedSprite("TileSheets\\animations", new Rectangle(128, 2948, 64, 64), 80f, 8, 0, new Vector2(position.X + 16f + (float)Game1.random.Next(-4, 4), position.Y + (float)(Game1.random.Next(-3, -1) * 4)), false, Game1.random.NextBool(), position.Y / 10000f, 0.03f, Color.Khaki * 0.45f, (float)(0.550000011920929 + (double)Game1.random.Next(-3, 4) * 0.0500000007450581), 0.0f, 0.0f, 0.0f);
-                        temporaryAnimatedSprite.delayBeforeAnimationStart = 20;
-                        Game1.currentLocation.temporarySprites.Add(temporaryAnimatedSprite);
+                        if ((__instance.currentSingleAnimation < 0 || __instance.currentSingleAnimation > 24) && (__instance.currentSingleAnimation < 96 || __instance.currentSingleAnimation > 120))
+                            return;
+                        if (owner.onBridge.Value && __instance.currentAnimationIndex % 2 == 0)
+                        {
+                            if (owner.currentLocation == Game1.currentLocation && Utility.isOnScreen(owner.Position, 384) && (owner == Game1.player || !LocalMultiplayer.IsLocalMultiplayer(true)))
+                                Game1.playSound("thudStep");
+                            owner.bridge?.OnFootstep(owner.Position);
+                            foreach (Trinket trinketItem in owner.trinketItems)
+                                trinketItem?.OnFootstep(owner);
+                        }
+                        if (__instance.currentAnimationIndex != 0 || owner.UniqueMultiplayerID != Game1.player.UniqueMultiplayerID)
+                            return;
+                        Game1.stats.takeStep();
                     }
-                    if (cueName != null && owner.currentLocation == Game1.currentLocation && Utility.isOnScreen(owner.Position, 384) && (owner == Game1.player || !LocalMultiplayer.IsLocalMultiplayer(true)))
-                    {
-                        Game1.playSound(cueName);
-                        if (owner.boots.Value != null && owner.boots.Value.ItemId == "853")
-                            Game1.playSound("jingleBell");
-                    }
-                    foreach (Trinket trinketItem in owner.trinketItems)
-                        trinketItem?.OnFootstep(owner);
-                    if (owner.UniqueMultiplayerID != Game1.player.UniqueMultiplayerID)
-                        return;
-                    Game1.stats.takeStep();
-                }
-                else
-                {
-                    if ((__instance.currentSingleAnimation < 0 || __instance.currentSingleAnimation > 24) && (__instance.currentSingleAnimation < 96 || __instance.currentSingleAnimation > 120))
-                        return;
-                    if (owner.onBridge.Value && __instance.currentAnimationIndex % 2 == 0)
-                    {
-                        if (owner.currentLocation == Game1.currentLocation && Utility.isOnScreen(owner.Position, 384) && (owner == Game1.player || !LocalMultiplayer.IsLocalMultiplayer(true)))
-                            Game1.playSound("thudStep");
-                        owner.bridge?.OnFootstep(owner.Position);
-                        foreach (Trinket trinketItem in owner.trinketItems)
-                            trinketItem?.OnFootstep(owner);
-                    }
-                    if (__instance.currentAnimationIndex != 0 || owner.UniqueMultiplayerID != Game1.player.UniqueMultiplayerID)
-                        return;
-                    Game1.stats.takeStep();
+
                 }
             }
         }
